a81ae70b0b709b755ed61d9aeee5997f3ee08b5c
ilyas <ilya_abramov_72@mail.ru>
2024-06-16 22:52:35 +0600

    added tests

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,67 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from unittest.mock import patch
+from .serializers import RegisterSerializer, LoginSerializer
 
-# Create your tests here.
+class MockRegisterSerializer(RegisterSerializer):
+    def save(self):
+        return True
+
+class MockLoginSerializer(LoginSerializer):
+    def get_jwt_token(self, validated_data):
+        return {'token': 'mocked_jwt_token'}
+
+class RegisterViewTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    @patch('your_app.views.RegisterSerializer', MockRegisterSerializer)
+    def test_register_view_success(self):
+        url = 'api/accounts/register'
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        
+        response = self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    @patch('your_app.views.RegisterSerializer', MockRegisterSerializer)
+    def test_register_view_failure(self):
+        url = 'api/accounts/register'
+        data = {}
+        
+        response = self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class LoginViewTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    @patch('your_app.views.LoginSerializer', MockLoginSerializer)
+    def test_login_view_success(self):
+        url = '/your_login_url/'
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        
+        response = self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)  
+
+    @patch('your_app.views.LoginSerializer', MockLoginSerializer)
+    def test_login_view_failure(self):
+        url = '/your_login_url/'
+        data = {}
+        
+        response = self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from unittest.mock import patch
+from .models import Post
+from .serializers import BlogSerializer
 
-# Create your tests here.
+class MockBlogSerializer(BlogSerializer):
+    def save(self):
+        post = Post.objects.create(**self.validated_data)
+        return post
+
+class MockJWTAuthentication:
+    def authenticate(self, request):
+        return (request.user, None)
+
+
